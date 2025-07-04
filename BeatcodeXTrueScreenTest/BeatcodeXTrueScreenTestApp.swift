2135fc60bd952f2e001452ecc2eba56c5641ff7e
Giovanni Fioretto <giovanni.fioretto.23@gmail.com>
2025-07-04 17:36:57 +0200

    Initial Commit

---
+++ BeatcodeXTrueScreenTest/BeatcodeXTrueScreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatcodeXTrueScreenTestApp.swift
+//  BeatcodeXTrueScreenTest
+//
+//  Created by Giovanni Fioretto on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BeatcodeXTrueScreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
