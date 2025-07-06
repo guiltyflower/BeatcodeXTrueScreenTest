8efd72858c2555ef9afb391ed98692a350fcd9da
Giovanni Fioretto <giovanni.fioretto.23@gmail.com>
2025-07-06 23:41:02 +0200

    Created a view to contain a single cell list to have cleaner code

--- BeatcodeXTrueScreenTest/View/ItemListView.swift
+++ BeatcodeXTrueScreenTest/View/ItemListView.swift
@@ -16,46 +16,7 @@ struct ItemListView: View {
             List {
                 ForEach($viewModel.items) { $item in
                     
-                    NavigationLink {
-                        ItemDetailView(item: $item)
-                    } label: {
-                        HStack {
-                            
-                            Text(item.itemName)
-                            Spacer()
-                            
-                            Button {
-                                item.isFavorite.toggle()
-                            } label: {
-                                Image(systemName: item.isFavorite ? "heart.fill" : "heart")
-                                    .foregroundColor(item.isFavorite ? .red : .gray)
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                            //I use this modifier to remove the click effect on the whole list item
-                            
-                        }
-                    }
-                    .accessibilityElement(children: .combine)
-                    
-                    .accessibilityHint(item.isFavorite ?
-                                       "This is a favorite item. Double tap to access the details page"
-                                       : "Double tap to access the details page")
-                    .accessibilityRemoveTraits(.isButton)
-                    .accessibilityAction(named: Text(item.isFavorite
-                                                     ? "Remove from favorites"
-                                                     : "Add to favorites")
-                    ) {
-                        item.isFavorite.toggle()
-                        let message = item.isFavorite
-                        ? "\(item.itemName) added to favorites"
-                        : "\(item.itemName) Removed from favorites"
-                        UIAccessibility.post(notification: .announcement, argument: message)
-                    }
-                    .accessibilityAction(named: Text("Spell item name")) {
-                        let spelled = item.itemName.map { String($0) }.joined(separator: " ")
-                        UIAccessibility.post(notification: .announcement, argument: spelled)
-                    }
-                    //putting the accessibility action I removed the spelling action (swipe down, so I've done it again in case of needing it, even if it's faster
+                    ItemCellView(item: $item)
                     
                 }
             }
